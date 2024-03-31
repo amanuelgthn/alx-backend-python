f342593ce96b0ff25b7f4bd6e12b3899a7f1c217
amanuelgthn <amanuelgthn@gmail.com>
2024-03-31 22:35:49 +0200

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """type_annotated function to conctact str1 and str2"""
+    return str1 + str2
