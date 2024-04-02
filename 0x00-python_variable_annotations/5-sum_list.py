6b6deb05178e6f1b90f4f6a577fb78fb1c0479b3
amanuelgthn <amanuelgthn@gmail.com>
2024-04-02 18:23:55 +0200

    update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,5 +2,8 @@
 """complex types-list of floats"""
 
 
-def sum_list(input_list: list[float]) -> int:
+def sum_list(input_list: list[float]) -> float:
+    """type annotated function sum_list which takes
+    input_list of floats as an argument and returns 
+    their sum as a float"""
     return sum(input_list)
