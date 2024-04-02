575b90f269939182e8bf202966d8e714501b6708
amanuelgthn <amanuelgthn@gmail.com>
2024-04-02 19:05:48 +0200

    update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """Complex types -string and int/float to tuple"""
 
 
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[float]]:
     """ function that takes string and an inr or float
     and returns a tuple"""
     return tuple([k, v * v])
