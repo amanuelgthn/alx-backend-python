228f6fdbaeb742623265c262a73c07eca71679a2
amanuelgthn <amanuelgthn@gmail.com>
2024-04-02 18:36:19 +0200

    6-sim_mixed_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,6 +7,6 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """type annotated function sum_list which takes
-    input_list of floats as an argument and returns 
+    input_list of floats as an argument and returns
     their sum as a float"""
     return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Complex types-mixed list"""
+
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> int:
+    return sum(mxd_lst)
