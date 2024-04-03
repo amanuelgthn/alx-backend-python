dcaae62d6871e3a4e997c58fd9a38ec02f3f4435
amanuelgthn <amanuelgthn@gmail.com>
2024-04-03 18:42:05 +0200

    func annotation update:

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,6 +6,7 @@ from typing import List, Tuple, Union
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """function to be validated using annotations"""
     zoomed_in: Tuple = [
         item for item in lst
         for i in range(factor)
