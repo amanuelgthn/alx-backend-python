7bea22a93e50695daf9e0712deda67d43b470fc9
amanuelgthn <amanuelgthn@gmail.com>
2024-04-03 13:20:42 +0200

    100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,11 +2,11 @@
 """Duck typing - first element of a sequence"""
 
 
-from types import NoneType
 from typing import Any, Sequence, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Code with the correct duck-typed annotations"""
     if lst:
         return lst[0]
     else:
