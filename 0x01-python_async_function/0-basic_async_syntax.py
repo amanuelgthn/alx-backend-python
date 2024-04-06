46ce61b1a21a8da5b799aa42aeabde368661862e
amanuelgthn <amanuelgthn@gmail.com>
2024-04-06 16:05:23 +0200

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ An asynchronous coroutine that takes an intergr nad returns
     the value it waited"""
     rand = random.uniform(0, max_delay)
