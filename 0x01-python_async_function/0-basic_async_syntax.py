1e23ebe99c596299f9a16b98a44b5b58feffb8ce
amanuelgthn <amanuelgthn@gmail.com>
2024-04-06 15:59:07 +0200

    Basic_async_function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basics of Python"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ An asynchronous coroutine that takes an intergr nad returns
+    the value it waited"""
+    rand = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return rand
