98c4646198dd3b8c9d84c9402a3ee5fdd1892bda
amanuelgthn <amanuelgthn@gmail.com>
2024-04-09 13:42:38 +0200

    Update sorted list return

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = [wait_random(max_delay) for i in range(n)]
     results = await asyncio.gather(*tasks)
-    return results
+    return sorted(results)
