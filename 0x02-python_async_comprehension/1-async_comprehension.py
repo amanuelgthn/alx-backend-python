e1206e56e6902e4c71dd73f0347337cda59f0437
amanuelgthn <amanuelgthn@gmail.com>
2024-04-18 14:18:10 +0200

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,5 +9,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """coroutine that takes no arguments and returns a list of float"""
     result = [i async for i in async_generator()]
     return result
